a17a990d4d525c4506b9465c2f9b4586289ae87c
ttetrafon <ttetrafon@yahoo.gr>
2024-05-24 00:31:24 +0300

    backup directories

--- src/main.py
+++ src/main.py
@@ -3,27 +3,32 @@ from pathlib import Path
 from typing import List
 
 source_directories: List[str] = [
-  'C:\\Users\\ttetr\\.aws',
-  'C:\\Users\\ttetr\\AppData\\Roaming\\Blender Foundation\\Blender',
-  'C:\\Users\\ttetr\\AppData\\LocalLow\\ZAUM Studio'
+  'C:/Users/ttetr/.aws',
+  'C:/Users/ttetr/AppData/Roaming/Blender Foundation/Blender',
+  'C:/Users/ttetr/AppData/LocalLow/ZAUM Studio'
 ]
 source_files: List[str] = [
-  'C:\\Users\\ttetr\\AppData\\Roaming\\Code\\User\\settings.json'
+  'C:/Users/ttetr/AppData/Roaming/Code/User/settings.json'
 ]
-target_directory: str = 'C:\\Users\\ttetr\\OneDrive\\Backup\\automated'
+target_directory: str = 'C:/Users/ttetr/OneDrive/Backup/automated'
 
 
 def main() -> None:
   for file in source_files:
     source = Path(file)
-    file_root = os.path.dirname(source)[2:]
-    file_name = os.path.basename(source)
-    destination = Path(target_directory + file_root + '//' + file_name)
+    root = os.path.dirname(source)[2:]
+    name = os.path.basename(source)
+    destination = Path(target_directory + root + '//' + name)
     os.makedirs(os.path.dirname(destination), exist_ok=True)
     shutil.copy(source, destination)
 
   for directory in source_directories:
-    None
+    source = Path(directory)
+    root = directory[3:]
+    target = Path(target_directory)
+    destination = target / root
+    # destination.mkdir(parents=True, exist_ok=True)
+    shutil.copytree(source, destination, dirs_exist_ok=True)
 
 
 if __name__ == '__main__':
